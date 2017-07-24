57ceb7138bf1b14102de35cbaa82c97ccd224f3f
Josh Young <j22cal@gmail.com>
2017-07-23 23:02:51 -0500

    Add more JS functions

--- javascript.js
+++ javascript.js
@@ -10,3 +10,27 @@ let getUrl = function() {
 let isNumber = function(num) {
   return (isFinite(num) && !isNaN(parseFloat(num)));
 }
+
+//Adopted from: https://codepen.io/gapcode/pen/vEJNZN:
+let isIE = (function() {
+  var ua = window.navigator.userAgent;
+  var rv = ua.indexOf('rv:');
+  var ie = parseInt(ua.substring(rv + 3, ua.indexOf('.', , rv)), 10);
+  if (ie == 11) {
+    document.getElementsByTagName('body')[0].className += ' ie';
+  }
+}());
+
+let theId = function(id) {
+  if (!document.getElementById(id)) { 
+    return false; 
+  }
+  return document.getElementById(id);
+}
+
+let theClass = function(cls) {
+  if (!document.getElementsByClassName(cls)) { 
+    return false; 
+  }
+  return document.getElementsByClassName(cls);
+}
