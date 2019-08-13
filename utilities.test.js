b2bde6c5d95ff2da29cd8f05b44e2a3b5f96e536
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-08-13 12:54:28 -0500

    Add testing for 'elementExists'

--- utilities.test.js
+++ utilities.test.js
@@ -1,4 +1,5 @@
 import { utilities } from './utilities';
+import { isTSAnyKeyword } from '@babel/types';
 
 describe('trim', () => {
   test('returns a string with the whitespace removed from either end', () => {
@@ -17,5 +18,17 @@ describe('isNumber', () => {
 
   test('returns false when a number is not passed in', () => {
     expect(utilities.isNumber("a")).toBe(false);
+  });
+});
+
+describe('elementExists', () => {
+  test('returns the correct element from the page', () => {
+    document.body.innerHTML = '<div><div id="my_element"></div></div>';
+    expect(utilities.elementExists("#my_element")).toBe(true);
+  });
+
+  test('does not return true if the element does not exist', () => {
+    document.body.innerHTML = '<div><div id="not_my_element"></div></div>';
+    expect(utilities.elementExists("#my_element")).toBe(false);
   });
 });
