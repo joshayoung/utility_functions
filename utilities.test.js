4c3156c70a7d56f1dad155f8d78775397797c72e
Josh Young <j22cal@gmail.com>
2019-08-13 17:24:48 -0500

    Re-order tests and add a test for 'getUrl'

--- utilities.test.js
+++ utilities.test.js
@@ -1,6 +1,18 @@
 import { utilities } from './utilities';
 import { isTSAnyKeyword } from '@babel/types';
 
+describe('elementExists', () => {
+  test('returns true if the element exists on the the page', () => {
+    document.body.innerHTML = '<div><div id="my_element"></div></div>';
+    expect(utilities.elementExists("#my_element")).toBe(true);
+  });
+
+  test('returns false if the element does not does not exist on the page', () => {
+    document.body.innerHTML = '<div><div id="not_my_element"></div></div>';
+    expect(utilities.elementExists("#my_element")).toBe(false);
+  });
+});
+
 describe('trim', () => {
   test('returns a string with the whitespace removed from either end', () => {
     expect(utilities.trim(" my value    ")).toMatch(/my value/);
@@ -11,6 +23,13 @@ describe('trim', () => {
   });
 });
 
+describe('getUrl', () => {
+  test('returns the URL from the current page', () => {
+    window.history.pushState({}, 'Test Title', 'http://localhost?seach=val');
+    expect(utilities.getUrl()).toMatch(/localhost/);
+  });
+});
+
 describe('isNumber', () => {
   test('returns true when a number is passed in', () => {
     expect(utilities.isNumber(1)).toBe(true);
@@ -18,17 +37,5 @@ describe('isNumber', () => {
 
   test('returns false when a number is not passed in', () => {
     expect(utilities.isNumber("a")).toBe(false);
-  });
-});
-
-describe('elementExists', () => {
-  test('returns the correct element from the page', () => {
-    document.body.innerHTML = '<div><div id="my_element"></div></div>';
-    expect(utilities.elementExists("#my_element")).toBe(true);
-  });
-
-  test('does not return true if the element does not exist', () => {
-    document.body.innerHTML = '<div><div id="not_my_element"></div></div>';
-    expect(utilities.elementExists("#my_element")).toBe(false);
   });
 });
